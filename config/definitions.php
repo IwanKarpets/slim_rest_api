5618af81e33788d3ff902107edb263445b1b8bf3
unknown <karpecivan@mail.ru>
2024-05-11 10:26:37 +0300

    change config database

--- config/definitions.php
+++ config/definitions.php
@@ -4,11 +4,18 @@ use App\Database;
 use App\Repositories\LoanRepository;
 
 
+$url = getenv("CLEARDB_DATABASE_URL") ? parse_url(getenv("CLEARDB_DATABASE_URL")) : null;
+
+$server = $url ? $url["host"] : '127.0.0.1';
+$username = $url ? $url["user"] : 'root';
+$password = $url ? $url["pass"] : '';
+$db = $url ? substr($url["path"], 1) : 'rest_api';
+
 
 return [
-    Database::class => function () {
+    Database::class => function () use ($server, $db, $username, $password) {
 
-        return new Database('127.0.0.1', 'rest_api', 'root', '');
+        return new Database($server, $db, $username, $password);
     },
     LoanRepository::class => function ($container) {
 
